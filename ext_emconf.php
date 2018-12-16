c177642fe4c6f00c6057855bf9a54a6c8cebe4bd
Nico de Haen <mail@ndh-websolutions.de>
2018-12-16 14:13:31 +0100

    Update TYPO3 dependency in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints'    => [
         'depends'   => [
             'autoloader' => '2.0.0-2.99.99',
-            'typo3'      => '7.6.0-8.7.99',
+            'typo3'      => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests'  => [],
